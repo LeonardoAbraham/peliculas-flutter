7788ac87ff29c8919bcee854b662cf11b38b906a
Leonardo <leon.alonzo.leo@gmail.com>
2022-09-06 13:03:52 -0500

    Peticiones HTTP y Mapear una peticion HTTP

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,8 +1,16 @@
 
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:peliculas/models/models.dart';
 
 class MoviesProvider extends ChangeNotifier{
+
+    String _apiKey = 'b4b44333678b34388f28450ef0799dbc';
+    String _baseUrl = 'api.themoviedb.org';
+    String _language = 'es-ES';
 
     MoviesProvider(){
         print('MoviesProvider inicializado');
@@ -11,7 +19,18 @@ class MoviesProvider extends ChangeNotifier{
     }
 
     getOnDisplayMovies() async {
-        print('getOnDisplayMovies');
+        var url =Uri.https( _baseUrl, '3/movie/now_playing', {
+            'api_key': _apiKey,
+            'language': _language,
+            'page': '1'
+        });
+
+        // Await the http get response, then decode the json-formatted response.
+        final response = await http.get(url);
+        
+        final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
+
+        print(nowPlayingResponse.results[1].title);
     }
 
 }
